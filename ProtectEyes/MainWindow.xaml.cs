fdf9ea7c0c8a2d9942a3c7a56b28a6090ac8069d
QH-PCOMPUTER\qiuhao <qiu66hao@163.com>
2019-09-14 20:55:55 +0800

    添加项目文件。

---
+++ ProtectEyes/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ProtectEyes
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MenuItem[] AreaMenuItems { get; private set; }
+        public bool CanClose { get; private set; }
+        public NotifyIcon NotifyIcon { get; private set; }
+
+        public List<NotifyWindow> NotifyWindows { get; set; } = new List<NotifyWindow>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitWindow();
+            Hide();
+        }
+
+        void InitWindow()
+        {
+            NotifyIcon = new NotifyIcon();
+            NotifyIcon.Text = "Protect Eye";
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                NotifyIcon.Icon = Resource.SunGlasses;
+            }
+
+            NotifyIcon.Visible = true;
+
+            NotifyIcon.DoubleClick += (sender, e) =>
+            {
+                if (IsVisible)
+                {
+                    Show();
+                }
+                else
+                {
+                    Hide();
+                }
+            };
+
+
+            AreaMenuItems = new[]
+            {
+                new MenuItem("Close", (sender, e)=>{
+                    ExitApp();
+                })
+            };
+
+            if (AreaMenuItems != null && AreaMenuItems.Length > 0)
+            {
+                NotifyIcon.ContextMenu = new ContextMenu(AreaMenuItems.ToArray());
+            }
+
+            Closing += MainWindow_Closing;
+
+            foreach (var scr in Screen.AllScreens)
+            {
+                var notifyWindow = new NotifyWindow(scr.WorkingArea);
+                notifyWindow.Show();
+                NotifyWindows.Add(notifyWindow);
+            }
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ExitApp()
+        {
+            foreach (var window in NotifyWindows)
+            {
+                if (window.IsVisible)
+                {
+                    window.Close();
+                }
+            }
+
+            NotifyIcon.Dispose();
+
+            Environment.Exit(0);
+        }
+
+        void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!CanClose)
+            {
+                Hide();
+                e.Cancel = true;
+            }
+            else
+            {
+                ExitApp();
+            }
+        }
+    }
+}
---
+++ ProtectEyes/NotifyWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace ProtectEyes
+{
+    /// <summary>
+    /// FullWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class NotifyWindow : Window
+    {
+        public Rectangle Area { get; set; }
+
+        public NotifyWindow(Rectangle area)
+        {
+            Area = area;
+            InitializeComponent();
+
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                var transform = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
+                var corner = transform.Transform(new System.Windows.Point(Area.Right, Area.Bottom));
+
+                Left = corner.X - ActualWidth;
+                Top = corner.Y - ActualHeight;
+            }));
+        }
+
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
