6b5bab152af5173893b080909e7a6a5308482c43
QH-PCOMPUTER\qiuhao <qiu66hao@163.com>
2019-09-15 12:21:12 +0800

    Fix UT

---
+++ ProtectEyeTest/NotifyViewModelTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using ProtectEyes;
+
+namespace ProtectEyeTest
+{
+    public class NotifyViewModelTest
+    {
+        [Test, RequiresSTA]
+        public void TestSetRestDesc()
+        {
+            var notifyModel = new NotifyViewModel(null, null);
+            notifyModel.SetRestDesc();
+            Assert.AreEqual("Please Take A Rest, Times Remain: 10s", notifyModel.RestDesc);
+        }
+    }
+}
